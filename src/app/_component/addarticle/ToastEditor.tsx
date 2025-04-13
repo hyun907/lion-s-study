a901903517e19b28fe50746530b248be8eab52c4
xionng <leeso0717@naver.com>
2025-04-13 22:03:08 +0900

    🌈fix: 에디터 본문 초기세팅값 렌더링 에러 해결

--- src/app/_component/addarticle/ToastEditor.tsx
+++ src/app/_component/addarticle/ToastEditor.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import styles from "./ToastEditor.module.css";
@@ -8,24 +7,22 @@ import styles from "./ToastEditor.module.css";
 const ToastEditor = () => {
   const editorRef = useRef<Editor>(null);
 
-  const handleGetContent = () => {
-    const instance = editorRef.current;
-    const markdown = instance?.getInstance().getMarkdown();
-    console.log("작성한 내용:", markdown);
-  };
+  useEffect(() => {
+    const instance = editorRef.current?.getInstance();
+    instance?.setMarkdown(""); // 본문 비우기
+  }, []);
 
   return (
     <div className={styles.editorWrapper}>
       <Editor
         ref={editorRef}
-        initialValue="내용을 작성하세요"
+        placeholder="내용을 작성하세요"
         previewStyle="vertical"
         height="40rem"
-        initialEditType="wysiwyg"
+        initialEditType="markdown"
         useCommandShortcut={true}
         className={styles.editorWrapper}
       />
-      {/* <button onClick={handleGetContent}>내용 확인</button> */}
     </div>
   );
 };
