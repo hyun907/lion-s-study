6aa8635da110432c37d467d19803eddb7ca16ea7
hyun907 <seunghyun020907@naver.com>
2025-04-14 14:42:28 +0900

    🎀style: 배경 및 레이아웃 퍼블리싱

--- src/app/(common-layout)/page.tsx
+++ src/app/(common-layout)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <p> Main 페이지</p>;
+import Image from "next/image";
+import backgroundImg from "@/assets/image/background_mask.png";
+import styles from "./page.module.css";
+
+export default function Main() {
+  return (
+    <div className={styles.pageContainer}>
+      <section className={styles.pageContent}>
+        <p className={styles.pageText}>숭실대학교 사자들을 위한 스터디 공간입니다.</p>
+      </section>
+      <div className={styles.column} />
+      <hr className={styles.row} />
+      <div className={styles.imgWrapper}>
+        <Image src={backgroundImg} alt="배경 이미지" unoptimized={true} />
+      </div>
+    </div>
+  );
 }
