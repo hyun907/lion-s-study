7ac5dbff0ee2b5a52fc02945eb28426865d3af61
yminjuu <ymj0580@naver.com>
2025-04-17 13:25:42 +0900

    💡feat: 스터디룸 backBtn 구현

--- src/app/_component/studyroom/StudyroomMain.tsx
+++ src/app/_component/studyroom/StudyroomMain.tsx
@@ -5,16 +5,21 @@ import Notice from "./Notice";
 import Link from "./Link";
 import Article from "./Article";
 import Ic_ArrowRight from "../../../assets/icon/arrow_right.svg";
+import { useRouter } from "next/navigation";
 
 // studyroom 메인 페이지
 const StudyroomMain = () => {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.push("/");
+  };
+
   return (
     <>
       <div className={styles.mainContainer}>
-        {/* mainContainer에 절대적인 뒤로가기 버튼 */}
-
         <div className={`${styles.subContainer} ${styles.leftContainer}`}>
-          <div className={styles.absoluteBackBtnContainer}>
+          <div className={styles.absoluteBackBtnContainer} onClick={handleGoBack}>
             <Ic_ArrowRight />
           </div>
           <StudyroomTitle />
