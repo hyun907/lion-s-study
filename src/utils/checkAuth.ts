725a33c75b187624ce9428c84b47167a9613255c
hyun907 <seunghyun020907@naver.com>
2025-05-01 00:38:36 +0900

    🌈fix: 충돌 수정

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -1,15 +1,7 @@
-import { useUserStore } from "@/store/useUserStore";
-
 export const checkAuth = () => {
   if (typeof document === "undefined") return false;
 
   const authToken = document.cookie.split("; ").find(row => row.startsWith("auth_token="));
 
-  if (!authToken) {
-    useUserStore.getState().clearUser();
-    localStorage.removeItem("user");
-    return false;
-  }
-
-  return true;
+  return !!authToken;
 };
