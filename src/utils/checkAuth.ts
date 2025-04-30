be7519f3ddb7f9aca014ba7482e9823d522c2b82
hyun907 <seunghyun020907@naver.com>
2025-04-30 20:55:29 +0900

    🌈fix: checkAuth 함수 document 사용 전 서버/클라이언트 분기 처리

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -1,4 +1,7 @@
 export const checkAuth = () => {
+  if (typeof document === "undefined") return false;
+
   const authToken = document.cookie.split("; ").find(row => row.startsWith("auth_token="));
+
   return !!authToken;
 };
